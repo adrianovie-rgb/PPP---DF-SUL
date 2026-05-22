ad1fefc1e25efd1a60173643573c113b662eb719
adrianovie-rgb <adriano_vie@hotmail.com>
2026-05-22 19:25:57 -0300

    Atualização automática Fri May 22 2026 00:00:00 GMT-0300 (Brasilia Standard Time) Sat Dec 30 1899 19:25:00 GMT-0306 (Brasilia Standard Time)

---
+++ data.js
@@ -0,0 +1,407 @@
+var D = {
+  "lastUpdate": "Fri May 22 2026 00:00:00 GMT-0300 (Brasilia Standard Time) Sat Dec 30 1899 19:25:00 GMT-0306 (Brasilia Standard Time)",
+  "meta": {
+    "ppp": 175
+  },
+  "regional": "DF-SUL",
+  "weeks": [
+    "W15",
+    "W16",
+    "W17",
+    "W18",
+    "W19",
+    "W20"
+  ],
+  "semana_ref": "W20",
+  "semana_w1": "W21",
+  "feriados": {
+    "W15": "Sexta-feira Santa (11/04)",
+    "W17": "Tiradentes (21/04)",
+    "W18": "Dia do Trabalhador (01/05)"
+  },
+  "node_order": [
+    "ECB8",
+    "EGO8",
+    "ERS8",
+    "ESB8",
+    "ESE8",
+    "EIJ8",
+    "EJO8",
+    "ELO8",
+    "EME8",
+    "EMR8",
+    "EOZ8",
+    "EUM8",
+    "EVE8"
+  ],
+  "nodes_ppp": {
+    "ECB8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EGO8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "ERS8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "ESB8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "ESE8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EIJ8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EJO8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "ELO8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EME8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EMR8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EOZ8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EUM8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EVE8": {
+      "grupo": "",
+      "tipo": "",
+      "data": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    }
+  },
+  "nodes_vol": {
+    "ECB8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "EGO8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "ERS8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "ESB8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "ESE8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "EIJ8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "EJO8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "ELO8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "EME8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "EMR8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "EOZ8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "EUM8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "EVE8": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ]
+  },
+  "nodes_w1": {
+    "ECB8": {
+      "ppp": null,
+      "vol": null
+    },
+    "EGO8": {
+      "ppp": null,
+      "vol": null
+    },
+    "ERS8": {
+      "ppp": null,
+      "vol": null
+    },
+    "ESB8": {
+      "ppp": null,
+      "vol": null
+    },
+    "ESE8": {
+      "ppp": null,
+      "vol": null
+    },
+    "EIJ8": {
+      "ppp": null,
+      "vol": null
+    },
+    "EJO8": {
+      "ppp": null,
+      "vol": null
+    },
+    "ELO8": {
+      "ppp": null,
+      "vol": null
+    },
+    "EME8": {
+      "ppp": null,
+      "vol": null
+    },
+    "EMR8": {
+      "ppp": null,
+      "vol": null
+    },
+    "EOZ8": {
+      "ppp": null,
+      "vol": null
+    },
+    "EUM8": {
+      "ppp": null,
+      "vol": null
+    },
+    "EVE8": {
+      "ppp": null,
+      "vol": null
+    }
+  },
+  "regional_ppp": {
+    "DF-SUL": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ]
+  },
+  "ppp_comparativo": {
+    "FM": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "DF-SUL": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "SP CAP": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "SP INT": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "RIMES-NE": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ]
+  }
+};
